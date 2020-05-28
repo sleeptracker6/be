75fb3dd947ad3c4df9905d8e9920a5c42abee6d4
CDuenas <chrisaduenas@gmail.com>
2020-05-28 00:40:14 -0700

    postgres cleanup

--- knexfile.js
+++ knexfile.js
@@ -44,10 +44,5 @@ module.exports = {
 		seeds: {
 			directory: "./data/seeds",
 		},
-		pool: {
-			afterCreate: (conn, done) => {
-				conn.run("PRAGMA foreign_keys = ON", done)
-			},
-		},
 	},
 }
